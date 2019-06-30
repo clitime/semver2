5a34506423b1f65ff3387b65b47c3d1f2cd99950
smirnov evgeniy <smirnov-eas@yandex.com>
2019-06-30 14:27:29 +0300

    feat(): build version whith hash code and date

--- src/semver.py
+++ src/semver.py
@@ -3,28 +3,17 @@ import re
 import json
 
 
-configuaration = {
-    'version_file': './',
-    'template_word': 'VERSION',
-    'branch_default': '',
-    'changelog_allow': False,
-    'changelog_path': './',
-    'template_path': './template/template',
-    'version_path': './version.c'
-}
-
-
-def parseArguments():
-    print("parseArguments")
-
-
 def getVersionFromVcs():
     """
     request from vcs tag like:
-    major.minor.patch[-pre-release][+metadata]-\d+
+    major.minor.patch[-pre-release][+metadata]-\\d+
     and parse it
     """
-    myCmd = os.popen('git describe --tags --long').read()
+    myCmd = ''
+    try:
+        myCmd = os.popen('git describe --tags --long').read()
+    except Exception as ex:
+        print('Exception a:', ex)
 
     pattern = r'\d+(\.\d+){2}(-[\w.]+)?(\+[\w.]+)?-\d+'
     match = re.search(pattern, myCmd)
@@ -36,77 +25,65 @@ def parseVcsVersion(vs):
     major - only digit require
     minor - only digit require
     patch - only digit require
-    pre-release - -[a-zA-Z0-9_\.] optional
-    metadata - +[a-zA-Z0-9_\.] optional
+    pre-release - -[a-zA-Z0-9_\\.] optional
+    metadata - +[a-zA-Z0-9_\\.] optional
     revision - digit require
     """
     match = re.search(
         r'(\d+)\.(\d+)\.(\d+)(?:-([\w.]+))?(?:\+([\w.]+))?-(\d+)',
         vs
         )
-    return match.groups()
+    return match.groups() if match else ''
 
 
 def buildVersionString(ver):
     """
       0     1      2      3             4       5
-    major.minor.patch[-pre-release][+metadata].\d+
+    major.minor.patch[-pre-release][+metadata].\\d+
     """
     semver = ''
     if ver:
         for (x, item) in list(zip(range(len(ver)), ver)):
-            if item:
-                sep = '.'
-                if x == 0:
-                    sep = ''
-                elif x == 3:
-                    sep = '-'
-                elif x == 4:
-                    sep = '+'
-                semver += sep + item
+            if item is None:
+                continue
+
+            sep = '.'
+            if x == 0:
+                sep = ''
+            elif x == 3:
+                sep = '-'
+            elif x == 4:
+                sep = '+'
+            elif x == 5 and item == '0':
+                continue
+            semver += sep + item
     else:
-        semver = 'fail_version'
+        semver = '0.0.0'
 
     return semver
 
 
 def getHashFromVcs():
     """get last commit hash and return 10 character"""
-    myCmd = os.popen('git show -s --format="%H %ad" --date=short').read()
-    print(myCmd)
-
-
-def saveVersionToBranch():
-    print("saveVersionToBranch")
-
-
-def getVcsLog():
-    # git log tagFrom...tagTo --pretty=format:'%h %ad | %s%d [%an]' --graph
-    # --date=short | grep "#changelog"
-    print("parseArguments")
-
-
-def archivePrj():
-    # git archive ...
-    print("archivePrj")
-
-
-def createChangelog():
-    print("createChangelog")
-
-
-def commitToBranch():
-    print("commitToBranch")
+    myCmd = ''
+    try:
+        myCmd = os.popen('git show -s --format="%H %ad" --date=short').read()
+    except Exception as ex:
+        print('Exception b:', ex)
+    hash_code = myCmd[:10]
+    date = myCmd[-11:].replace('-', '').strip()
+    return hash_code + ' ' + date
 
 
 def buildVersionFile(cfg):
     ver_striing = getVersionFromVcs()
     match = parseVcsVersion(ver_striing)
     str_ver = buildVersionString(match)
-    buildFromTemplate(cfg, str_ver)
+    hash_date = getHashFromVcs()
+    buildFromTemplate(cfg, str_ver, hash_date)
 
 
-def buildFromTemplate(cfg, str_ver):
+def buildFromTemplate(cfg, str_ver, hash_date):
     template = open(cfg['template_path'], 'r')
 
     ver_file = open(cfg['version_path'], 'w')
@@ -117,23 +94,34 @@ def buildFromTemplate(cfg, str_ver):
                 str_ver,
                 line
             )
+        line = re.sub(
+                '{{' + cfg['template_hash'] + '}}',
+                hash_date,
+                line
+            )
         ver_file.write(line)
 
     template.close()
     ver_file.close()
 
 
-def readConfig(cfg):
-    config_file = open("config.json", "r")
-    config = json.load(config_file)
-    config_file.close()
+def readConfig():
+    config = {
+        "template_path": "./version_template.c",
+        "template_word": "SEMVER",
+        "template_hash": "HASH_DATA",
+        "version_path": "./version.c"
+    }
+    try:
+        with open("config.json", "r") as config_file:
+            config = json.load(config_file)
+    except OSError as ex:
+        print('ERROR read configuration.', ex)
+    except Exception as ex:
+        print(ex)
 
-    for c in config:
-        if c in cfg:
-            cfg[c] = config[c]
+    return config
 
 
 if __name__ == '__main__':
-    readConfig(configuaration)
-
-    buildVersionFile(configuaration)
+    buildVersionFile(readConfig())
